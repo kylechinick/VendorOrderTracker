651ffaa3b732b9d0ac959f836ec82cc379595600
Kyle Chinick <kylechinick@icloud.com>
2022-07-22 15:27:18 -0700

    Add route to display add orders to vendor

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -37,5 +37,18 @@ namespace VendorOrderTracker.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderPrice, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
