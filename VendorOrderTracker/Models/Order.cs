1744b8a8f7b5cbf28e5e04248f1a5cec5391553b
Kyle Chinick <kylechinick@icloud.com>
2022-07-22 10:26:20 -0700

    Add RED test for GetDescription_ReturnsDescription_String()

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,14 +5,12 @@ namespace VendorOrderTracker.Models
   public class Order
   {
     public string Title { get; set; }
-    public string Description { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
 
-    public Order(string description, string title)
+    public Order(string title)
     {
       Title = title;
-      Description = description;
       _instances.Add(this);
       Id = _instances.Count;
     }
