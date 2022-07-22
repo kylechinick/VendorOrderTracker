950acf07713a6c44b6fe455f1abc1e4d36dda044
Kyle Chinick <kylechinick@icloud.com>
2022-07-22 16:49:00 -0700

    Add parameter to hold vendor description

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -6,12 +6,14 @@ namespace VendorOrderTracker.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string VendorName { get; set; }
+    public string VendorDescription { get; set; }
     public int Id { get; }
     public List<Order> Orders { get; set; }
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string vendorDescription)
     {
       VendorName = vendorName;
+      VendorDescription = vendorDescription;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
