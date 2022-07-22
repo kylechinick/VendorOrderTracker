00b01c93a6fbffe95fc9868cfda9f8bc3b02dd1a
Kyle Chinick <kylechinick@icloud.com>
2022-07-22 09:25:26 -0700

    Initial commit

---
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    // TBD
+  }
+}
---
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    // TBD
+  }
+}
