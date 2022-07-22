2f5e138dce8fc77d04884ef1d88e941ffaed2856
Kyle Chinick <kylechinick@icloud.com>
2022-07-22 15:42:02 -0700

    Add route to order detail page

--- VendorOrderTracker/Controllers/OrdersController.cs
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -12,5 +12,16 @@ namespace VendorOrderTracker.Controllers
       Vendor vendor = Vendor.Find(vendorId);
       return View(vendor);
     }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
   }
 }
