d5cd059a8f9a063df3e55a1045c114f3168023ae
Kyle Chinick <kylechinick@icloud.com>
2022-07-22 09:50:27 -0700

    Initial commit

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      // TBD
+    }
+  }
+}
---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      // TBD
+    }
+  }
+}
